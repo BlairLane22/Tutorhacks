0c4e69e03c48da9252b5e164517d065b672d12ff
Blair <blairjacklane@gmail.com>
2018-08-14 19:24:20 -0400

    Got the sign up flow working

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,17 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def after_sign_in_path_for(resource)
-    # if resource.userid == 'teacher'
-    #   teacher_path(resource.id)
-    # else
-    #   student_path(resource.id)
-    # end
-    new_tutor_path(resource.id)
+    if resource.rank == 'Tutor'
+      new_tutor_path(resource.id)
+    else
+      new_student_path(resource.id)
+    end
   end
 
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :rank)
   end
 
   def account_update_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :tutor
+
+
+  Rank = ['Tutor', 'Student']
 end
