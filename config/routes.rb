09ecf6917fa4c140b054fef7d24a63c70b52e926
Blair <blairjacklane@gmail.com>
2018-11-03 17:58:55 -0400

    Getting stuff done

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
 
 
   get 'home/index'
+  get 'home/pricing'
+  get 'home/about'
   get 'student/parent'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
